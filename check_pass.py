4ca993602ac7e8a62e6e5170b7b7c29cc61ebfa9
softwarelordcode <softwarelordcode@users.noreply.github.com>
2025-06-06 12:20:03 +0200

    verify a single pass

---
+++ check_pass.py
@@ -0,0 +1,24 @@
+'''
+This script checks if a password has been exposed in data breaches using the 
+"Have I Been Pwned" API.
+It uses the k-anonymity model to protect user privacy.
+It sends a request to the API with the first 5 characters of the SHA-1 hash of the password.
+'''
+import hashlib
+import requests
+
+password = "12345"
+hashed_pass = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+url = "https://api.pwnedpasswords.com/range/" + hashed_pass[:5]  
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    print("Password has been pwned!")
+    for line in response.text.splitlines():
+        hash_part = line.split(":")[0]
+        count = line.split(":")[1]
+        print(f"Hash: {hash_part}, Count: {count}")
+else:
+    print("Password is \"safe\".")
+    print(response.text)
