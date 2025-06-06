4bb7ff09ff112cbade1abd1fdedb80d02b732a7e
softwarelordcode <softwarelordcode@users.noreply.github.com>
2025-06-06 13:13:34 +0200

    user input

--- check_pass.py
+++ check_pass.py
@@ -4,21 +4,29 @@ This script checks if a password has been exposed in data breaches using the
 It uses the k-anonymity model to protect user privacy.
 It sends a request to the API with the first 5 characters of the SHA-1 hash of the password.
 '''
+import sys
 import hashlib
 import requests
 
-password = "12345"
+try:
+    password = sys.argv[1]
+except IndexError:
+    print("Usage: python check_pass.py <pass>")
+    sys.exit(1)
+
 hashed_pass = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
 url = "https://api.pwnedpasswords.com/range/" + hashed_pass[:5]
 
 response = requests.get(url,timeout=10)
 
 if response.status_code == 200:
-    print("Password has been pwned!")
     for line in response.text.splitlines():
         hash_part = line.split(":")[0]
         count = line.split(":")[1]
-        print(f"Hash: {hash_part}, Count: {count}")
+        if hashed_pass == hashed_pass[:5] + hash_part:
+            print("Password has been pwned!")
+            print(f"Your password has been found {count} times in data breaches.")
+            break
 else:
     print("Password is \"safe\".")
     print(response.text)
